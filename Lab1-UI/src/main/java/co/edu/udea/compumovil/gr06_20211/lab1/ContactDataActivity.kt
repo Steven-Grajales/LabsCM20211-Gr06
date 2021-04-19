2385519c192b89597ecd5d25e91b8335a87e9869
Emanuel Gaviria <emanuel.ge11@gmail.com>
2021-04-18 22:14:21 -0500

    Portrait layout de ContactData, versión inicial

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20211/lab1/ContactDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr06_20211.lab1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactDataActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+    }
+}
